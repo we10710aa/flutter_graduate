46408fdc189e1241182935af66950d3bcf1f2d3c
raymondchang <xrayjemmy@gmail.com>
2019-05-06 23:02:44 +0800

    fixing web issue on ios(2)

--- lib/fish_collection.dart
+++ lib/fish_collection.dart
@@ -1,9 +1,10 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_graduate/fish_json_converter.dart';
 import 'package:flutter_graduate/web_page.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class FishCollectionWidget extends StatefulWidget {
   @override
@@ -15,28 +16,42 @@ class FishCollectionWidget extends StatefulWidget {
 class _FishCollectionWidgetState extends State<FishCollectionWidget> {
   FishCollection _fishCollection;
   static final BASE_URL = 'http://fishdb.sinica.edu.tw/mobi/species.php?id=';
+
   Widget _listItemBuilder(BuildContext context, int index) {
     List<Fish> list = _fishCollection.fishs;
     return Padding(
       padding: const EdgeInsets.only(bottom: 8, left: 8),
       child: GestureDetector(
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder:(context)=>WebViewExample()));
+        onTap: () async{
+          //This is a workaround because flutter webview doesn't seen to work with http
+          String url = BASE_URL + list[index].id;
+          if (Platform.isAndroid) {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => WebViewPage(url)));
+          }
+          else if(Platform.isIOS){
+            if(await canLaunch(url)){
+              await launch(url);
+            }
+          }
         },
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
+            CachedNetworkImage(
+              imageUrl: list[index].imgUrl,
+              placeholder: (context, url){
+                return Container(
+                  width: 160,
+                  height: 100,
+                  child:Center(child: CircularProgressIndicator()));},
+              errorWidget: (context, url, error) => Icon(Icons.error),
+              fit: BoxFit.cover,
               width: 160,
               height: 100,
-              child: CachedNetworkImage(
-                imageUrl: list[index].imgUrl,
-                placeholder: (context,url)=>CircularProgressIndicator(),
-                errorWidget: (context,url,error)=>Icon(Icons.error),
-                fit: BoxFit.cover,
-              ),
             ),
-            SizedBox(width: 8,),
+            SizedBox(
+              width: 8,
+            ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
@@ -66,8 +81,7 @@ class _FishCollectionWidgetState extends State<FishCollectionWidget> {
           return ListView.builder(
             itemBuilder: _listItemBuilder,
           );
-        }
-        else{
+        } else {
           return Center(
             child: CircularProgressIndicator(),
           );
